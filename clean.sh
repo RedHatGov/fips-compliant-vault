ba50c44e56250a3b867ce3d61a3a4910cdc3831b
rlucente <rlucente@redhat.com>
2017-01-05 15:51:34 -0500

    clean start

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pushd `dirname $0` 2>&1 > /dev/null
-WORKDIR=`pwd`
-popd 2>&1 > /dev/null
-
-rm -fr $WORKDIR/nss_pbkdf2/*.so $WORKDIR/target
-cd $WORKDIR/fips-compliant-vault/
-mvn clean
--- fips-compliant-vault/src/main/java/org/jboss/security/fips/tools/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author rlucente
- *
- */
-package org.jboss.security.fips.tools;
